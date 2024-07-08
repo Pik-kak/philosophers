e2abeb06eeb2cf3d0bba22859e250799f939eab0
pik-kak <karokauh@gmail.com>
2024-07-08 13:57:15 +0300

    added init_forks()

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkauhane <kkauhane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pikkak <pikkak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 14:21:15 by pikkak            #+#    #+#             */
-/*   Updated: 2024/07/05 13:39:17 by kkauhane         ###   ########.fr       */
+/*   Updated: 2024/07/08 13:56:41 by pikkak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,29 @@ void	init_philos(t_philo *philos, t_data *data, pthread_mutex_t *forks)
 	i = 0;
 	while (i < data->philos_count)
 	{
-		philos[i]->id = 
-		philos[i]->last_ate =
-		philos[i]->meals_count =
-		philos[i]->dead =
+		philos[i]->id = i + 1;
+		philos[i]->eating = 0;
+		philos[i]->start = get_current_time();
+		philos[i]->last_ate = get_current_time();
+		philos[i]->meals_count = 0;
 
 		i++;	
 	}
 }
 
-void	init_data(struct t_data *data, char **argv)
+void	init_forks(pthread_mutex_t *forks, int philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+}
+
+void	init_data(struct t_data *data, struct t_philo *philos, char **argv)
 {
 	data->philos = ft_atoi(argv[1]);
 	data->time_to_die = ft_atoi(argv[2]);
@@ -38,21 +51,40 @@ void	init_data(struct t_data *data, char **argv)
 		data->meals = ft_atoi(argv[5]);
 	else
 		data->meals = -1;
+	data->drink_poison = 0;
+	data->philos = philos;
+	pthread_mutex_init(&data->dead_lock, NULL);
+	pthread_mutex_init(&data->write_lock, NULL);
+	pthread_mutex_init(&data->meal_lock, NULL);
 }
 
 int main(int argc, char **argv)
 {
-	t_data	data;
-	t_philo	philos[PHILOS_MAX]
+	t_data			data;
+	t_philo			philos[PHILOS_MAX]
 	pthread_mutex_t	forks[PHILOS_MAX]
-	int		i;
+	int				i;
 	
-	i = 1;
-	if(argc < 5 || argc > 6)
-		printf("Wrong amount of arguments");
-	if (check_args(args) == 1)
-		return (1);
 	i = 0;
-	init_data(&data, argv);
-	init_philos(&philos, &data, &forks);
+	if(argc < 5 || argc > 6)//return 1
+		error("Wrong amount of arguments");
+	else if (check_args(args) == 1)//return 1
+		error("Wrong amount of arguments");
+	else
+	{
+		init_data(&data, philos, argv);
+		init_forks(forks, ft_atoi(argv[1]));
+		init_philos(&philos, &data, &forks);//should this make the treads as well?
+		while ()
+		{
+			if (pthread_create(&philos[i], NULL, &routine, NULL) != 0)
+				error("Could not create a thread\n");
+		}
+		while ()
+		{
+			pthread_join(philos[i], NULL);
+		}
+	}
+	return (0);
 }
+
